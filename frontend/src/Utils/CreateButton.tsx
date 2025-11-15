5d94fe848c8abfbe1bd2acb009d683230cda1aa7
Hitesh Navare <navarehitesh65@gmail.com>
2025-11-15 23:26:54 +0530

    added the added the open setIsOpen state on createbutton onclick

--- frontend/src/Utils/CreateButton.tsx
+++ frontend/src/Utils/CreateButton.tsx
@@ -1,13 +1,19 @@
+"use client"
+import { useState } from "react";
+import DialogModule from "./DialogModule";
 
 function CreateButton() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div>
     <button className="p-[3px] relative">
      <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-     <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+     <div onClick={() => setIsOpen(true)} className="px-8 py-2  bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
       Start a Stream as Creator
      </div>
     </button>
+    <DialogModule isOpen={isOpen} setIsOpen={setIsOpen} />
     </div>
   )
 }
