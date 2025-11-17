2e9ea08646311c089176a2b3c1ac54b5d663dfb5
Hitesh Navare <navarehitesh65@gmail.com>
2025-11-17 15:27:23 +0530

    added yt link adding ui and frontend logic

--- frontend/src/Utils/Stream.tsx
+++ frontend/src/Utils/Stream.tsx
@@ -1,11 +1,58 @@
 import { useParams } from "react-router-dom"
+import SongList from "./SongList";
+import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/button";
+import { useState } from "react";
+
+    let link = 'https://youtu.be/mbJVNt1TY5Y?si=cUEBZNJU6iVSTkyx'
+    const link2 = 'https://youtu.be/SQg2g_uelIo?si=W_ZYFttr5-Upbcc2'
+    const link3 = 'https://youtu.be/SQg2g_uelIo?si=_-EDjfkfjHjLzWZb'
+    const song = 'mbJVNt1TY5Y?si=9V7UtYXtaKFvMQbd';
 
 export default function Stream() {
-    const { id } = useParams();
+    const [inputLink, setInputLink] = useState('');
+    const [songLink, setSongLink] = useState('');
+
+    const addSong = () => {
+      convertLinkIntoEmbed(inputLink);
+    }
+
+    const convertLinkIntoEmbed = (ytLink: string) => {
+      const extracted = ytLink.split('be/')
+      console.log("extracted song link",extracted[1]);
+      //backend call{}
+      setSongLink(extracted[1]);
+    }
     
   return (
-    <div>
-        {id};
+    <div className="flex overflow-hidden bg-[#0a0a0a]">
+        <div className="w-[50vw] h-screen p-32">
+            <SongList />
+        </div>
+
+        <div className="w-[50vw] p-32  flex-col flex gap-5">
+
+           <div className="flex gap-6">
+              <Input value={inputLink} onChange={(e) => setInputLink(e.target.value)} placeholder="Enter YT Link" className="text-white font-sans font-mediums"/>
+              <Button onClick={addSong} className="bg-gradient-to-r from-purple-600 to-violet-800 hover:opacity-80">Add Song</Button>
+          </div>    
+          <div className="border h-[40vh] bg-white">
+
+              <iframe
+                width="702"
+                height="376"
+                src={`https://www.youtube.com/embed/${songLink}`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded youtube"
+              />
+          </div>
+          <div>
+             <Button className="w-full bg-gradient-to-r from-purple-600 to-violet-800 hover:opacity-80">Play Next</Button>
+          </div>
+         
+        </div>
     </div>
   )
 }
